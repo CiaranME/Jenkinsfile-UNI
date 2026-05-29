3d12a6d4a3aec240ee995ec21b852be06dfbb6a9
Ciaran <58226913+CiaranME@users.noreply.github.com>
2026-05-29 12:25:46 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return f"Hello! I'm currently running in {os.environ.get('ENV', 'development')}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
