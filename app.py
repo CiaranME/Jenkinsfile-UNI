6b9c559a13010412b68f5db08d136b0fa8751b18
Ciaran <58226913+CiaranME@users.noreply.github.com>
2026-05-29 13:48:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return f"goodbye! I'm currently running in {os.environ.get('ENV', 'development')}"
+    return f"Hello I'm currently running in {os.environ.get('ENV', 'development')}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
